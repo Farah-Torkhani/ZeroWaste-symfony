25fc7db186b85dae3f4470f4290dc6bbb8a55baa
elghoul10 <80488733+elghoul10@users.noreply.github.com>
2023-02-24 07:21:17 +0100

    Update Participation.php

--- src/Entity/Participation.php
+++ src/Entity/Participation.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ParticipationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ParticipationRepository::class)]
@@ -12,7 +14,8 @@ class Participation
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+    
+    #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $verification_p = null;
 
@@ -27,6 +30,14 @@ class Participation
 
     #[ORM\Column(length: 255)]
     private ?string $image = null;
+
+    #[ORM\ManyToMany(targetEntity: Collecte::class, inversedBy: 'participations')]
+    private Collection $collecte_id;
+
+    public function __construct()
+    {
+        $this->collecte_id = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -89,6 +100,37 @@ class Participation
     public function setImage(string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Collecte>
+     */
+    public function getCollecteId(): Collection
+    {
+        return $this->collecte_id;
+    }
+    
+    public function setCollecteId(int $collecte_id): self
+    {
+        $this->collecte_id = $collecte_id;
+
+        return $this;
+    }
+    
+    public function addCollecteId(Collecte $collecteId): self
+    {
+        if (!$this->collecte_id->contains($collecteId)) {
+            $this->collecte_id->add($collecteId);
+        }
+
+        return $this;
+    }
+
+    public function removeCollecteId(Collecte $collecteId): self
+    {
+        $this->collecte_id->removeElement($collecteId);
 
         return $this;
     }
